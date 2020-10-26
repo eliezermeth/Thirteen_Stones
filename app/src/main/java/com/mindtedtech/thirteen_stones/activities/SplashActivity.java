9745e6499b1333a73c187bd496f824f7bde0e771
emeth <eliezeraameth@gmail.com>
2020-10-25 20:18:46 -0400

    Added drawables, set icon and created SplashActivity.java

---
+++ app/src/main/java/com/mindtedtech/thirteen_stones/activities/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.mindtedtech.thirteen_stones.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
