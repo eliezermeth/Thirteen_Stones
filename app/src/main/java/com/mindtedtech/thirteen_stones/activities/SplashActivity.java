11d435f2ada5507517fed9980db19ff2eed5aa04
emeth <eliezeraameth@gmail.com>
2020-10-25 20:34:30 -0400

    Running blank program.

--- app/src/main/java/com/mindtedtech/thirteen_stones/activities/SplashActivity.java
+++ app/src/main/java/com/mindtedtech/thirteen_stones/activities/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.mindtedtech.thirteen_stones.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,5 +12,11 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setDefaultNightMode(
+                Build.VERSION.SDK_INT < 28 ? AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY : AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
     }
 }
