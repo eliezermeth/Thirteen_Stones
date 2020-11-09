ab9fc039489d600d3acc8ed96f4dbc202e127bfe
Eliezer Meth <63016889+eliezermeth@users.noreply.github.com>
2020-11-08 20:45:29 -0500

    After Mod7 videos.

--- app/src/main/java/com/mindtedtech/thirteen_stones/activities/StatisticsActivity.java
+++ app/src/main/java/com/mindtedtech/thirteen_stones/activities/StatisticsActivity.java
@@ -1,5 +1,6 @@
 package com.mindtedtech.thirteen_stones.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,10 +13,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.mindtedtech.thirteen_stones.R;
+import com.mindtedtech.thirteen_stones.models.ThirteenStones;
+
+import java.util.Locale;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    private TextView tvDataGamesPlayed,
+            tvDataPlayer1Wins, tvDataPlayer1WinsPercent,
+            tvDataPlayer2Wins, tvDataPlayer2WinsPercent;
+
+    private ThirteenStones mCurrentGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +34,15 @@ public class StatisticsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_statistics);
         setupToolbar();
         setupFAB();
+        setupViews();
+        getIncomingData();
+        processAndOutputIncomingData();
     }
 
     private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null)
+        if (getSupportActionBar() !=null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -37,10 +51,41 @@ public class StatisticsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onBackPressed();
             }
         });
+    }
+
+    private void setupViews() {
+        tvDataGamesPlayed = findViewById(R.id.tv_data_games_played);
+        tvDataPlayer1Wins = findViewById(R.id.tv_data_player1_wins);
+        tvDataPlayer1WinsPercent = findViewById(R.id.tv_data_player1_win_percent);
+        tvDataPlayer2Wins = findViewById(R.id.tv_data_player2_wins);
+        tvDataPlayer2WinsPercent = findViewById(R.id.tv_data_player2_win_percent);
+    }
+
+    private void getIncomingData() {
+        Intent intent = getIntent();
+        String gameJSON = intent.getStringExtra("GAME");
+        mCurrentGame = ThirteenStones.getGameFromJSON(gameJSON);
+    }
+
+    private void processAndOutputIncomingData() {
+        final String FORMAT_STRING = "%2.1f%%", N_A = "N/A";
+        int numberOfGamesPlayed = mCurrentGame.getNumberOfGamesPlayed();
+        if (!mCurrentGame.isGameOver() && numberOfGamesPlayed > 0)
+            numberOfGamesPlayed--;
+        int p1Wins = mCurrentGame.getNumberOfWinsForPlayer(1);
+        int p2Wins = mCurrentGame.getNumberOfWinsForPlayer(2);
+        String p1WinPct = numberOfGamesPlayed  == 0 ? N_A :
+                String.format(Locale.US, FORMAT_STRING, (p1Wins/(double)numberOfGamesPlayed)*100);
+        String p2WinPct = numberOfGamesPlayed == 0 ? N_A :
+                String.format(Locale.US, FORMAT_STRING, (p2Wins/(double)numberOfGamesPlayed)*100);
+        tvDataGamesPlayed.setText(String.valueOf(numberOfGamesPlayed));     // don't forget String.valueOf()
+        tvDataPlayer1Wins.setText(String.valueOf(p1Wins));
+        tvDataPlayer2Wins.setText(String.valueOf(p2Wins));
+        tvDataPlayer1WinsPercent.setText(p1WinPct);
+        tvDataPlayer2WinsPercent.setText(p2WinPct);
     }
 
     @Override
@@ -48,8 +93,7 @@ public class StatisticsActivity extends AppCompatActivity {
         if (item.getItemId() == android.R.id.home) {
             onBackPressed();
             return true;
-        }
-        else
+        } else
             return super.onOptionsItemSelected(item);
     }
 }
